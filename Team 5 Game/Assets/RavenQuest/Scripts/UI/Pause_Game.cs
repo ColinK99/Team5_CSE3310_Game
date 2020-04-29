050281e869078610e6d5b9ccf230c6ec73e1b622
ColinK99 <colin.kirch99@yahoo.com>
2020-04-29 15:35:04 -0500

    RavenQuestv3.3

---
+++ Team 5 Game/Assets/RavenQuest/Scripts/UI/Pause_Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Game : MonoBehaviour
+{
+    public GameObject otherButton;
+    // Start is called before the first frame update
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        this.gameObject.SetActive(false);
+        otherButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        this.gameObject.SetActive(false);
+        otherButton.SetActive(true);
+    }
+}
