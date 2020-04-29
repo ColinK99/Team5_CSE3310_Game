ef1f173946546dd5122ab329521f9b19618f2469
ColinK99 <colin.kirch99@yahoo.com>
2020-04-29 15:52:36 -0500

    RavenQuestv3.4

--- Team 5 Game/Assets/RavenQuest/Scripts/UI/Pause_Game.cs
+++ Team 5 Game/Assets/RavenQuest/Scripts/UI/Pause_Game.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause_Game : MonoBehaviour
 {
     public GameObject otherButton;
+    public Button jumpButton;
+    public Button strikeButton;
+    public GameObject move;
     // Start is called before the first frame update
     public void Resume()
     {
         Time.timeScale = 1f;
         this.gameObject.SetActive(false);
+        jumpButton.enabled = true;
+        strikeButton.enabled = true;
+        move.GetComponent<FixedJoystick>().enabled = true;
         otherButton.SetActive(true);
     }
 
@@ -18,6 +25,9 @@ public class Pause_Game : MonoBehaviour
     {
         Time.timeScale = 0f;
         this.gameObject.SetActive(false);
+        jumpButton.enabled = false;
+        strikeButton.enabled = false;
+        move.GetComponent<FixedJoystick>().enabled = false;
         otherButton.SetActive(true);
     }
 }
